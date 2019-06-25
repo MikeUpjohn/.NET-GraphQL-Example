6c1bae8ffbbee5c7926c00558ce934b6247bb423
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2019-06-25 19:36:15 +0100

    Graph QL Example started

---
+++ GraphQLExample/Controllers/GraphQLController.cs
@@ -0,0 +1,16 @@
+﻿using GraphQLExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GraphQLExample.Controllers
+{
+    [Route("api/[controller]")]
+    public class GraphQLController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> GraphQLPost([FromBody] GraphQLQuery query)
+        {
+            return Ok();
+        }
+    }
+}
